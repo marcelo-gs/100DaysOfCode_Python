958f906d8374c83034972f605d9d9e0db50a4e7a
Marcelo Gomes <marcelo.gs@terra.com.br>
2020-12-23 15:43:14 -0300

    feat(higher lower): include codes of the project.

---
+++ Day014/higher_lower.py
@@ -0,0 +1,65 @@
+from game_data import data
+from art import logo, vs
+import random
+from os import system, name 
+
+used_data = []
+score = 0 
+def Inicite_game():
+    clear()
+    print(logo)
+    return False
+
+def get_aleatory_info():
+    find = False
+    position = 0
+    while not find:
+        position = random.randint(0,len(data)-1)
+        name = data[position]["name"]
+        if name not in used_data:
+            used_data.append(name)
+            find = True
+            return data[position]
+
+def print_dispute(dataA, dataB):
+    print(f"Compare A: {dataA['name']}, {dataA['description']} from {dataA['country']}")
+    print(vs)
+    print(f"Against B: {dataB['name']}, {dataB['description']} from {dataB['country']}")
+
+def who_wins(dataA, dataB):
+    if int(dataA['follower_count']) > int(dataB['follower_count']):
+        return "A"
+    else:
+        return "B"
+
+# define our clear function 
+def clear(): 
+  
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+  
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
+
+
+data_A = get_aleatory_info()
+data_B = get_aleatory_info()
+
+end_of_game = Inicite_game()
+while not end_of_game:
+    print_dispute(data_A, data_B)
+    answer = input("\nWho has more followers (Type 'A' or 'B'): ").upper()
+    if answer == who_wins(data_A, data_B):
+        print("Win")
+        if answer == "B":
+            data_A = data_B
+        end_of_game = Inicite_game()
+        data_B = get_aleatory_info()
+        score += 1
+        print(f"You're right! Current score: {score}")
+        
+    else:
+        print(f"Sorry, that's wrong. Final Score: {score}")
+        end_of_game = True
