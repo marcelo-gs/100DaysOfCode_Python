b0a9fbbc1745dae29f661dde794fb531108918d2
Marcelo Gomes <marcelo.gs@terra.com.br>
2020-12-24 12:28:54 -0300

    feat: exercises for day016

---
+++ Day016/Exercises/exercise01.py
@@ -0,0 +1,9 @@
+from turtle import Turtle, Screen, position
+
+timmy = Turtle()
+timmy.shape('turtle')
+timmy.color("blue")
+timmy.circle(100)
+
+my_Screen = Screen()
+my_Screen.exitonclick()        
