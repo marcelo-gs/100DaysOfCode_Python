f099916c7e8f7096697a1daf04dca654de06ab58
Marcelo Gomes <marcelo.gs@terra.com.br>
2020-12-30 23:20:31 -0300

    feat: High Score funcionality

--- Day020-021/SnakeGame/scoreboard.py
+++ Day020-021/SnakeGame/scoreboard.py
@@ -7,6 +7,8 @@ class ScoreBoard(Turtle):
     def __init__(self) -> None:
         super().__init__()
         self.color("white")
+        with open("data.snake") as data:
+            self.high_score = int(data.read())
         self.penup()
         self.hideturtle()
         self.goto(0,270)
@@ -20,7 +22,15 @@ class ScoreBoard(Turtle):
     def game_over(self):
         self.goto(0,0)
         self.write(arg="GAME OVER", move=False, align=ALIGMENT, font=FONT)
+        self.reset()
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.snake",'w') as data:
+                data.write(f"{self.high_score}")
+            self.score = 0
 
     def refresh(self):
         self.clear()
-        self.write(arg=f"Score: {self.score}", move=False, align=ALIGMENT, font=FONT)
+        self.write(arg=f"Score: {self.score} High Score: {self.high_score}", move=False, align=ALIGMENT, font=FONT)
