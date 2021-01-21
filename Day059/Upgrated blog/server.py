125d1385c3a23ffbb67ddfb157763c2434620ccc
Marcelo Gomes <marcelo.gs@terra.com.br>
2021-01-21 19:44:12 -0300

    feat: complete contact form from the blog

--- Day059/Upgrated blog/server.py
+++ Day059/Upgrated blog/server.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
-import random, datetime, requests
+from flask import Flask, render_template, request
+import random, datetime, requests, smtplib
 
 
 app = Flask(__name__)
 blog = None
+
+OWN_EMAIL = "YOUR OWN EMAIL ADDRESS"
+OWN_PASSWORD = "YOUR EMAIL ADDRESS PASSWORD"
 
 @app.route('/')
 def index():
@@ -16,9 +19,13 @@ def index():
 def about():
     return render_template("about.html")
 
-@app.route('/contact')
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
 
 @app.route('/post/<int:post_id>')
 def get_post(post_id):
@@ -35,6 +42,15 @@ def get_post(post_id):
         if artitle['id'] == post_id:
             final_article = artitle
     return render_template("post.html", post=final_article)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Contact\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
